0715623fb1ddb42f7efe8747fd10ec68ca9dd1d3
stonasss <luis.b.santos96@gmail.com>
2023-03-31 23:37:40 -0300

    patch: corrected imports to userRoutes for login

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,8 @@ const userRoutes = Router();
 
 userRoutes.post("/signup/patient", validateSchema(userSchema), userControllers.createPatient);
 userRoutes.post("/signup/doctor", validateSchema(userSchema), userControllers.createDoctor);
-userRoutes.post("/signin", userControllers.signin);
+userRoutes.post("/signin/patient", userControllers.signinPatient);
+userRoutes.post("/signin/doctor", userControllers.signinDoctor);
 /* userRoutes.get("/", userControllers.get);
 userRoutes.put("/:id", userControllers.put);
 userRoutes.delete("/:id", userControllers.delete); */
